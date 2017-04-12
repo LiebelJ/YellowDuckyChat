99e44892e847a8e8ab3e56b6af0f465030a8f780
Julien <julien-liebel@hotmail.com>
2017-04-12 19:21:55 -0400

    fix

--- frontend/src/app/chatroom/chatroom.component.ts
+++ frontend/src/app/chatroom/chatroom.component.ts
@@ -17,9 +17,9 @@ export class ChatroomComponent implements OnInit {
 
   public initChatrooms(){
     this.chatrooms=[
-      {"id":"1","name":"Chat 1","img":"http://www.yellowducky.co/YellowDuckySmall.png"},
-      {"id":"2","name":"Chat 2","img":"http://www.yellowducky.co/YellowDuckySmall.png"},
-      {"id":"3","name":"Chat 3","img":"http://www.yellowducky.co/YellowDuckySmall.png"}
+      {"id":"1","name":"Chat 1","img":"http://dev.yellowducky.co/YellowDuckySmall.png"},
+      {"id":"2","name":"Chat 2","img":"http://dev.yellowducky.co/YellowDuckySmall.png"},
+      {"id":"3","name":"Chat 3","img":"http://dev.yellowducky.co/YellowDuckySmall.png"}
     ];
   }
 
