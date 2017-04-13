68443fee6125e9179601ff569f8e3e111a80a95f
Julien <julien-liebel@hotmail.com>
2017-04-13 16:01:05 -0400

    update

--- frontend/src/app/about/about-resolve.service.ts
+++ frontend/src/app/about/about-resolve.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { User } from '../shared/models/user';
 import { UserService } from '../shared/services/user.service';
 
 @Injectable()
-export class AboutUsersResolve implements Resolve<User> {
+export class AboutUsersResolve implements Resolve<User[]> {
 
   constructor(private service: UserService) {}
 
-  resolve(route: ActivatedRouteSnapshot) {
+  resolve(route: ActivatedRouteSnapshot,state: RouterStateSnapshot) {
     return this.service.getUsers().then(users => users);
   }
 
