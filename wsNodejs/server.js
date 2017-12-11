16d3107f7f4db43c531c01cf962d931a43525bdc
Liebel, J <julien-liebel@hotmail.com>
2017-12-11 12:56:14 -0500

    Update server.js
    Added new domain

--- wsNodejs/server.js
+++ wsNodejs/server.js
@@ -24,7 +24,7 @@ wsServer = new WebSocketServer({
 
 function originIsAllowed(origin) {
     // put logic here to detect whether the specified origin is allowed. 
-    valid_origins = ['http://localhost:4200','http://localhost:9000','http://aws.yellowducky.co'];
+    valid_origins = ['http://localhost:4200','http://localhost:9000','http://aws.yellowducky.co',http://aws.stillz.net];
     console.log('Valid origins: ' + valid_origins);
     if (valid_origins.indexOf(origin) != -1) {
         console.log('Connection accepted from origin ' + origin);
